0db3b69aab85fea9d29d19e19fa65625a81d6821
Kris Moore <kris@ixsystems.com>
2020-11-16 13:54:48 -0500

    Create and setup properly the backend directory

--- post_install.sh
+++ post_install.sh
@@ -11,6 +11,10 @@ SKEY=$(cat /root/skey)
 sysrc -f /etc/rc.conf minio_enable="YES"
 sysrc -f /etc/rc.conf minio_env="MINIO_ACCESS_KEY=$AKEY MINIO_SECRET_KEY=$SKEY"
 
+# Setup backend directory
+mkdir -p /var/db/minio
+chown -R minio /var/db/minio && chmod u+rxw /var/db/minio
+
 # Start the service
 service minio start 2>/dev/null
 
